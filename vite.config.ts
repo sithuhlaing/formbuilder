a029ca89492e173cd4477b6e175c98ba5cd1340d
Si Thu Hlaing <lukephew@gmail.com>
2025-08-25 17:42:59 +0200

    update and changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Form-Builder/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/formbuilder/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
