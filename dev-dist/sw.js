fe6ee60343fc5c60680f919931efd12281f3171d
Si Thu Hlaing <lukephew@gmail.com>
2025-11-01 15:57:35 +0100

    it is updated

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sb1i9j80654"
+    "revision": "0.dl38dps35n"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
