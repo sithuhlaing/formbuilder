1062ab13f6129d4efe078d0586ab70a50e169e2c
Si Thu Hlaing <lukephew@gmail.com>
2026-06-28 11:31:53 +0800

    fixed url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/Form-Builder/',
+  base: '/formbuilder/',
   plugins: [react()],
   resolve: {
     alias: {
